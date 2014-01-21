698056aa383759c9394b4092df198239762d05b8
Solosoft <solosoft.nl@gmail.com>
2014-01-21 20:07:42 +0100

    ListExtention

---
+++ Solosoft.Lib/Helpers/ListHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solosoft.Lib.Helpers
+{
+   public static class ListHelpers
+    {
+       //shuffles an Ilist collection of T
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            var _random = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                var k = _random.Next(n + 1);
+                T value = list [k];
+                list [k] = list [n];
+                list [n] = value;
+            }
+
+        }
+
+    }
+}
